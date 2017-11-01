1e70a83e00761eaa0b0a698d64680020d6f61858
cr <1501261740@qq.com>
2017-11-01 09:03:06 +0800

    指令测试

--- src/app/attribute/attribute.directive.ts
+++ src/app/attribute/attribute.directive.ts
@@ -4,8 +4,10 @@ import { Directive, ElementRef, AfterViewInit, Renderer2, HostBinding, HostListe
 	selector: '[myButton]'
 })
 export class AttributeDirective implements AfterViewInit {
-
+	//获取输入
 	@Input('myButton') name: string;
+	//模版元素
+	@Input() temp:HTMLElement;
 
 	constructor(
 		private er: ElementRef,
@@ -13,11 +15,26 @@ export class AttributeDirective implements AfterViewInit {
 	) { }
 	
 	ngAfterViewInit() {
-		console.log(this.renderer2, this.er)
+		console.log('注入的ng类',this.renderer2, this.er)
+		
+		//增加类名
 		this.renderer2.addClass(this.er.nativeElement, 'btn');
-	}
 
+		//增加元素
+		let spanEle = document.createElement('span');
+		spanEle.innerHTML = '插入的元素';
+		this.renderer2.appendChild(this.er.nativeElement,spanEle);
+
+		//获取页面上的元素
+		console.log('获取的输入元素',this.temp);
+		this.renderer2.appendChild(this.er.nativeElement,this.temp);
+
+		//
+		this.renderer2.createElement('');
+	}
+	//绑定属性
 	@HostBinding('style.font-size') fontSize = '20px';
+	//绑定事件
 	@HostListener('click') onclick() {
 		alert('你输入了' + this.name);
 	}
